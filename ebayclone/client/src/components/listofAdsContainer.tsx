9d1570a572df159e5d759a54d10e4494506c88dd
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-13 12:37:46 +0100

    setup react

---
+++ ebayclone/client/src/components/listofAdsContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListofAdsContainer extends React.Component <any> {
+    render() {
+      return (
+        <div>
+            <p>something</p>
+        </div>
+      );
+    }
+  }
+
+export default ListofAdsContainer;
