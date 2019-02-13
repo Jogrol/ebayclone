217ee7a5b50f69332009e0295d5dc0a5385e9654
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-13 11:23:56 +0100

    setup db table

---
+++ ebayclone/server/src/advertisements/entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity } from 'typeorm/repository/BaseEntity'
+
+@Entity()
+export default class Ads extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('text', {nullable:false})
+  title: string
+  @Column('text', {nullable:false})
+  description: string
+  @Column('text', {nullable:false})
+  picture: string
+  @Column('text', {nullable:false})
+  price: number
+  @Column('text', {nullable:false})
+  email: string
+  @Column('text', {nullable:false})
+  phone_number: number
+}
